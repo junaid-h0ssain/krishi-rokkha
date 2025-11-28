e4edcfd0d89156d8bdffb29f55f906a224e93a16
junaid-hossain <158271661+junaid-h0ssain@users.noreply.github.com>
2025-11-28 22:14:55 +0600

    main router added

--- src/main.js
+++ src/main.js
@@ -1,24 +1,33 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// main.js
+import { initAuth } from "../modules/auth.js";
+import { initProfile } from "../modules/profile.js";
+import { initBatches } from "../modules/batches.js";
+import { initWeather } from "../modules/weather.js";
+import { initAiScanner } from "../modules/aiScan.js";
+import { initOffline } from "../modules/offline.js";
+import { initUI } from "../modules/ui.js";
+import { auth } from "./firebase-config.js";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+let currentUser = null;
 
-setupCounter(document.querySelector('#counter'))
+function setCurrentUser(user) {
+    currentUser = user;
+}
+
+function getCurrentUser() {
+    return currentUser;
+}
+
+window.HG = { getCurrentUser }; // simple global accessor
+
+async function bootstrap() {
+    initUI();
+    initOffline();
+    initAuth(setCurrentUser);
+    initProfile();
+    initBatches();
+    initWeather();
+    initAiScanner();
+}
+
+bootstrap();
