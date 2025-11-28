a648b1b8a21f291ffec15595623134fb1be82cd0
Fardina <155743091+fardina-tahsin@users.noreply.github.com>
2025-11-28 22:05:49 +0600

    configs added

---
+++ src/config.js
@@ -0,0 +1,20 @@
+// config.js
+export const CLOUDINARY_CONFIG = {
+    cloudName: import.meta.env.VITE_CLOUDINARY_CLOUD_NAME,
+    uploadPreset: import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET
+};
+
+export const HF_CONFIG = {
+    apiUrl: import.meta.env.VITE_HF_API_URL,
+    apiKey: import.meta.env.VITE_HF_API_KEY
+};
+
+export const WEATHER_CONFIG = {
+    baseUrl: import.meta.env.VITE_WEATHER_BASE_URL,
+    apiKey: import.meta.env.VITE_WEATHER_API_KEY
+};
+
+export const RF_CONFIG = {
+    apiUrl : import.meta.env.VITE_RF_API_URL,
+    apiKey: import.meta.env.VITE_RF_API_KEY
+}
